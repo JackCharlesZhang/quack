dec88f05ab1639939f398519dda54228062abf48
Jack Zhang <jackzhang@Jacks-MacBook-Pro-2.local>
2025-08-18 21:42:57 -0700

    add

--- quack/symmetric_dense_gemm_sm90.py
+++ quack/symmetric_dense_gemm_sm90.py
@@ -1950,7 +1950,7 @@ def _symmetric_dense_gemm(
         c is not None,          
         persistent,           
         (M, K, L),
-        (a.stride(1) == 1, b.stride(1) == 1, c.stride(1) == 1 if c is not None else None)
+        (a.stride(1) == 1, b.stride(1) == 1, c.stride(1) == 1 if c is not None else None, d.stride(1) == 1)
     )
 
     if persistent:
