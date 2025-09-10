03743600eabf14178439f20293ded3fb5cf87a2f
Jack Zhang <jackzhang@Jacks-MacBook-Pro-4.local>
2025-09-10 12:03:15 -0400

    add

--- tests/test_rmsnorm.py
+++ tests/test_rmsnorm.py
@@ -60,11 +60,12 @@ def test_rmsnorm_forward_backward(M, N, input_dtype, weight_dtype, eps, use_comp
     x_ref = x.detach().clone().requires_grad_()
     weight_ref = weight.detach().clone().requires_grad_()
     function = torch.compile(rmsnorm, fullgraph=True) if use_compile else rmsnorm
-    out = function(x, weight, eps=eps)
+    out, residual_out = function(x, weight, eps=eps)
     out_ref = rmsnorm_ref(x_ref, weight_ref, eps=eps)
 
     print(out)
-    print(out_ref)
+    print(residual_out)
+
     assert out.shape == x.shape
     assert out.dtype == input_dtype
     torch.testing.assert_close(out, out_ref, atol=atol, rtol=1e-3)
