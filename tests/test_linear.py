4140e8164556691e156cf39e02f8fe7125afc08a
Jack Zhang <jackzhang@Jacks-MacBook-Pro-4.local>
2025-09-28 17:17:54 -0400

    add

--- tests/test_linear.py
+++ tests/test_linear.py
@@ -33,6 +33,7 @@ def test_linear(in_features, out_features, input_dtype):
     out_pt = F.linear(x, w)
     print(out[:20])
     print(out_ref[:20])
+    print(out.size(), out_ref.size())
     assert (out - out_ref).abs().max() < 2 * (out_pt - out_ref).abs().max() + 1e-6
     dout = torch.randn_like(out)
     dx, dw = torch.autograd.grad(out, (x, w), dout)
