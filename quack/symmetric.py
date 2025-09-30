ab5d0a3464341631e3d9c95a06fd12fdf99bb3b8
Jack Zhang <jackzhang@Jacks-MacBook-Pro-4.local>
2025-09-30 16:58:31 -0400

    add

--- quack/symmetric.py
+++ quack/symmetric.py
@@ -85,8 +85,7 @@ def gemm_symmetric(
     max_active_clusters = get_max_active_clusters(cluster_M * cluster_N) if persistent else 0
     GemmWrapperBase.create_cute_tensors({k: v for k, v in tensor_infos.items() if k != "PostAct"}, major_configs)
 
-    print("HI")
-    tensor_infos["PostAct"].cute_tensor = GemmWrapperBase.create_transposed_cute_tensor(tensor_infos["PostAct"].cute_tensor, tensor_infos["PostAct"].major, major_configs["PostAct"])
+    tensor_infos["PostAct"].cute_tensor = GemmWrapperBase.create_transposed_cute_tensor(tensor_infos["PostAct"].tensor, tensor_infos["PostAct"].major, major_configs["PostAct"])
     
     
     def scalar_arg(scalar: float | Tensor):
