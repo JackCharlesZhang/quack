0090074ed1bcd2c12f95f852b930dabf9ca2fe6c
Tri Dao <tridpq@gmail.com>
2026-03-08 23:44:23 +0700

    [DSL] Refactor ParamsBase and ArgumentsBase

--- quack/cute_dsl_utils.py
+++ quack/cute_dsl_utils.py
@@ -44,46 +44,50 @@ def get_device_capacity(device: torch.device = None) -> Tuple[int, int]:
     return torch.cuda.get_device_capability(device)
 
 
+def _partition_fields(obj):
+    """Split dataclass fields into (constexpr_dict, non_constexpr_dict) by type."""
+    all_fields = {field.name: getattr(obj, field.name) for field in fields(obj)}
+    constexpr = {n: f for n, f in all_fields.items() if isinstance(f, StaticTypes)}
+    non_constexpr = {n: f for n, f in all_fields.items() if not isinstance(f, StaticTypes)}
+    return constexpr, non_constexpr
+
+
+def _new_from_mlir_values(self, values):
+    constexpr_fields, non_constexpr_fields = _partition_fields(self)
+    for (name, field), n_items in zip(non_constexpr_fields.items(), self._values_pos):
+        non_constexpr_fields[name] = cutlass.new_from_mlir_values(field, values[:n_items])
+        values = values[n_items:]
+    return self.__class__(**non_constexpr_fields, **constexpr_fields)
+
+
 @dataclass
 class ParamsBase:
     def __extract_mlir_values__(self):
-        all_fields = [getattr(self, field.name) for field in fields(self)]
-        non_constexpr_fields = [f for f in all_fields if not isinstance(f, StaticTypes)]
+        _, non_constexpr_fields = _partition_fields(self)
         values, self._values_pos = [], []
-        for obj in non_constexpr_fields:
+        for obj in non_constexpr_fields.values():
             obj_values = cutlass.extract_mlir_values(obj)
             values += obj_values
             self._values_pos.append(len(obj_values))
         return values
 
-    def __new_from_mlir_values__(self, values):
-        all_fields = {field.name: getattr(self, field.name) for field in fields(self)}
-        constexpr_fields = {n: f for n, f in all_fields.items() if isinstance(f, StaticTypes)}
-        non_constexpr_fields = {
-            n: f for n, f in all_fields.items() if not isinstance(f, StaticTypes)
-        }
-        for (name, field), n_items in zip(non_constexpr_fields.items(), self._values_pos):
-            non_constexpr_fields[name] = cutlass.new_from_mlir_values(field, values[:n_items])
-            values = values[n_items:]
-        return self.__class__(**non_constexpr_fields, **constexpr_fields)
+    __new_from_mlir_values__ = _new_from_mlir_values
 
 
 @dataclass
 class ArgumentsBase(JitArgument):
     def __c_pointers__(self):
-        all_fields = [getattr(self, field.name) for field in fields(self)]
-        non_constexpr_fields = [f for f in all_fields if not isinstance(f, StaticTypes)]
+        _, non_constexpr_fields = _partition_fields(self)
         c_ptrs = []
-        for obj in non_constexpr_fields:
+        for obj in non_constexpr_fields.values():
             if hasattr(obj, "__c_pointers__"):
                 c_ptrs.extend(obj.__c_pointers__())
         return c_ptrs
 
     def __get_mlir_types__(self):
-        all_fields = [getattr(self, field.name) for field in fields(self)]
-        non_constexpr_fields = [f for f in all_fields if not isinstance(f, StaticTypes)]
+        _, non_constexpr_fields = _partition_fields(self)
         types, self._values_pos = [], []
-        for obj in non_constexpr_fields:
+        for obj in non_constexpr_fields.values():
             if hasattr(obj, "__get_mlir_types__"):
                 obj_types = obj.__get_mlir_types__()
                 types.extend(obj_types)
@@ -92,13 +96,4 @@ class ArgumentsBase(JitArgument):
                 self._values_pos.append(0)
         return types
 
-    def __new_from_mlir_values__(self, values):
-        all_fields = {field.name: getattr(self, field.name) for field in fields(self)}
-        constexpr_fields = {n: f for n, f in all_fields.items() if isinstance(f, StaticTypes)}
-        non_constexpr_fields = {
-            n: f for n, f in all_fields.items() if not isinstance(f, StaticTypes)
-        }
-        for (name, field), n_items in zip(non_constexpr_fields.items(), self._values_pos):
-            non_constexpr_fields[name] = cutlass.new_from_mlir_values(field, values[:n_items])
-            values = values[n_items:]
-        return self.__class__(**non_constexpr_fields, **constexpr_fields)
+    __new_from_mlir_values__ = _new_from_mlir_values
